598daafe566100077c7ee409b02a3e9ac8b4d435
David Stanek <dstanek@dstanek.com>
2018-05-07 09:51:16 -0400

    Documentation update

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{api:0,definit:0,index:0,look:0,modul:0,page:0,search:0,spec:0,take:0},titles:["Welcome to LOL\u2019s documentation!"],titleterms:{document:0,indic:0,lol:0,tabl:0,welcom:0}})
